45b445c36362adf44e5b3a0f2c4a2ffb22ce2f8e
sevonthren <scythe7137@gmail.com>
2025-09-20 14:09:48 +0530

    added important ccomment

--- src/main/java/stos/tutorialmod/Tutorialmod.java
+++ src/main/java/stos/tutorialmod/Tutorialmod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import stos.tutorialmod.item.ModItems;
 
 //very important comment
 public class Tutorialmod implements ModInitializer {
@@ -13,5 +14,6 @@ public class Tutorialmod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
+        ModItems.registerMainItems();
 	}
 }
