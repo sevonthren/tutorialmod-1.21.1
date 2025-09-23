4e8cc6349ebe368286be5124f93662b01c0c6ec0
sevonthren <scythe7137@gmail.com>
2025-09-23 19:27:34 +0530

    base level implementation of - pinkgarnet, rawpinkgarnet, pinkgarnetblock, pinkgarnetore

--- src/main/java/stos/tutorialmod/block/ModBlocks.java
+++ src/main/java/stos/tutorialmod/block/ModBlocks.java
@@ -3,6 +3,7 @@ package stos.tutorialmod.block;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -10,6 +11,8 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.IntProvider;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import stos.tutorialmod.Tutorialmod;
 
 public class ModBlocks {
@@ -20,7 +23,8 @@ public class ModBlocks {
                     .sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
     public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
-            new Block(AbstractBlock.Settings.create()
+            new ExperienceDroppingBlock(UniformIntProvider.create(2,5),
+                    AbstractBlock.Settings.create()
                     .strength(3f)
                     .requiresTool()
                     .sounds(BlockSoundGroup.STONE)));
